41cd9a7128abca5acbf4f6c10d3fad98289b1ed5
anju <anjualfino@gmail.com>
2023-04-13 12:57:18 +0800

    adding app changes

---
+++ demo/app.js
@@ -0,0 +1,115 @@
+
+let timeEl = document.querySelector("#time");
+let questionElem = document.querySelector("#questtitle");
+let choicesElem = document.querySelector("#choices");
+let ans1El = document.querySelector("#ans1");
+let ans2El = document.querySelector("#ans2");
+let ans3El = document.querySelector("#ans3");
+let ans4El = document.querySelector("#ans4");
+let message2 = document.getElementById("m2");
+let message3 = document.getElementById("m3");
+let textarea = document.getElementById("m4");
+let signUpButton = document.querySelector("#submitinitials");
+
+
+
+//adding event listner to the start button.
+document.getElementById("startButton").addEventListener(
+    "click",
+    () => {
+        document.getElementById("facecard").hidden = true;
+        document.getElementById("viewscores").hidden = true;
+        document.getElementById("time").hidden = false;
+        document.getElementById("wrapper").hidden = false;
+        startTime();
+    },
+    false
+);
+
+//adding timer with 40 seconds,4 questions and for negative answers decrese 10 seconds.
+let timeLeft = 20;
+function startTime() {
+    // let timeLeft = TIME_LIMIT;
+    const timer = setInterval(() => {
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            // Code to handle when time runs out
+            showScore();
+        } else {
+            timeEl.textContent = timeLeft;
+            timeLeft--;
+            showQuestion();
+            console.log(currentQuestion);
+        }
+    }, 1000);
+}
+
+
+
+let currentQuestion = 0;
+let score = 0;
+
+function showQuestion() {
+    const current = questions[currentQuestion];
+    questionElem.textContent = current.title;
+
+    choicesElem.innerHTML = "";
+    for (let i = 0; i < current.choices.length; i++) {
+        const choice = document.createElement("button");
+        choice.innerText = current.choices[i];
+        choicesElem.addEventListener("click", function (e) {
+            var clickedChoice = e.target.innerText;
+            checkAnswer(clickedChoice);
+            //console.log("Button clicked!");
+        });
+        choicesElem.appendChild(choice);
+
+    }
+}
+
+
+function checkAnswer(clickedans) {
+    // const selected = event.target.getAttribute("data-index");
+    let currentanswer = questions[currentQuestion].answer;
+    if (clickedans == currentanswer) {
+        score++;
+    }
+    else {
+        timeLeft = timeLeft - 10;
+    }
+
+    currentQuestion++;
+    if (currentQuestion >= questions.length) {
+        showScore();
+    }
+    else {
+        console.log("here")
+        showQuestion();
+    }
+}
+
+
+function showScore() {
+    document.getElementById("time").hidden = true;
+    document.getElementById("wrapper").hidden = true;
+    document.getElementById("displayscore").hidden = false;
+
+    message2.textContent = "Your final score is " + score;
+    message3.textContent = "Enter initials: ";
+
+
+
+    signUpButton.addEventListener("click", function (event) {
+        // event.preventDefault();
+
+        var initials = textarea.value;
+
+        if (initials != "") {
+
+            // TODO: Save email and password to localStorage and render the last registered user
+            localStorage.setItem("initials", initials);
+            localStorage.setItem("score", score);
+            //renderLastRegistered();
+        }
+    });
+}
---
+++ demo/questions.js
@@ -0,0 +1,25 @@
+const questions = [
+    {
+        title: "question 1?",
+        choices: ["ans11", "ans2", "ans3", "ans4"],
+        answer: "ans11"
+    },
+
+    {
+        title: "question 2?",
+        choices: ["ans1", "ans22", "ans3", "ans4"],
+        answer: "ans22"
+    },
+
+    {
+        title: "question 3?",
+        choices: ["ans1", "ans2", "ans33", "ans4"],
+        answer: "ans33"
+    },
+    {
+        title: "question 4?",
+        choices: ["ans1", "ans2", "ans3", "ans44"],
+        answer: "ans44"
+    }
+]
+
